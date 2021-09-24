7d6a4f654149855baedde56329be869fa72b5ba4
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-09-24 21:29:26 +0600

    Add files via upload

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,40 @@
+/*                       Author : Hadiur Rahman Nabil
+                         ID     : 20-42095-1
+                         Course : Algorithms [L]
+*/
+                  
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+const int MAX= 100010;
+
+int main()
+{
+    int minimum,n,j,i;
+
+    vector<int>v(MAX);
+
+    cout<<"Number Of The Array Elements :\n";
+    cin>>n;
+    cout<<"Enter The Elements :\n";
+    for(int i=0;i<n;i++)
+    {
+       cin>>v[i];
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j =0;j<n-1;j++)
+         {
+            if(v[j]>v[j+1])
+              swap(v[j],v[j+1]);
+         }
+        cout<<"After "<< i+1 <<" Times Swapping Array Elements Are :\n";
+        for(j =0;j<n;j++)
+           cout<<v[j]<<" \n";
+       
+   }
+}
